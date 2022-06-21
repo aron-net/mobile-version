74600ae8b7d1e625a88c146e23aa88b1aa944f4e
aron-helu <aronmilahela@gmail.com>
2022-06-21 03:21:26 +0300

    Fixing the dynamic page of hamburger

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-console.log(hamburgerMenu);
+
 const navigation = document.querySelector('.frame-navigation');
 
 const closeMenu = document.querySelector('.close-menu');
 
-const show =() => {
-    navigation.setAttribute('data-visible', true)
-    console.log(show)
-}
+const show = () => {
+    navigation.setAttribute('data-visible', true);
+};
+
 hamburgerMenu.addEventListener('click', show);
 
-console.log(show)
+const closeShow = () => {
+    navigation.setAttribute('data-visible', false);
+};
+
+closeMenu.addEventListener('click', closeShow);
+
+const nav = document.querySelectorAll('.nav');
+
+nav.forEach((n) => n.addEventListener('click', closeShow));
