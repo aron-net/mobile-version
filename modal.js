ad8e6ef458a57506893d2d2dd8925c4a3f6f20f2
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-06-22 13:11:37 -0600

    do dynamic display of html using object

---
+++ modal.js
@@ -0,0 +1,30 @@
+const recentWorkContainer = document.getElementById('multi-post-grid');
+const recentWork = [
+    {
+        picFile: 'work1.png',
+        project_title: 'Multi-Post Stories',
+        description: 'A daily section of privately personalized reads; no accounts or sign-ups required. has been the industry&#x27;s standards dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
+        skill: ['css', 'html', 'bootstrap', 'Ruby']
+    },
+    {
+        picFile: 'work2.png',
+        project_title: 'Multi-Post Stories',
+        description: 'A daily section of privately personalized reads; no accounts or sign-ups required. has been the industry&#x27;s standards dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
+        skill: ['css', 'html', 'bootstrap', 'Ruby']
+    },
+    {
+        picFile: 'work3.png',
+        project_title: 'Multi-Post Stories',
+        description: 'A daily section of privately personalized reads; no accounts or sign-ups required. has been the industry&#x27;s standards dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
+        skill: ['css', 'html', 'bootstrap', 'Ruby']
+    },
+    {
+        picFile: 'work4.png',
+        project_title: 'Multi-Post Stories',
+        description: 'A daily section of privately personalized reads; no accounts or sign-ups required. has been the industry&#x27;s standards dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
+        skill: ['css', 'html', 'bootstrap', 'Ruby']
+    }
+]
+const loadRecentWork = () => {
+    
+}
